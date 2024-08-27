65435c42d63467c836846f9c3da272086ff249de
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-27 19:50:29 +0500

    learning

--- src/index.js
+++ src/index.js
@@ -2,32 +2,26 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./index.css";
 
+const author = "John Doe";
+const title = 'Interesting facts for curious minds';
+const img = './images/book-1.jpg';
+
 const BookList = () => {
-    return <section className="booklist">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-    </section>
-}
+    return (<section className="booklist">
+        <Book author={author} title={title} img={img} />
+        <Book author={author} title={title} img={img} />
+    </section>)
+};
 
-const Book = () => {
-    return <article className="book">
-        <Image />
-        <Title />
-        <Author />
-    </article>
-}
+const Book = (props) => {
+    console.log(props);
+    return (<article className="book">
+        return <img src={props.img} alt={props.title} />
+        return <h2>{props.title}</h2>
+        <h4>{props.author.toUpperCase()}</h4>
+    </article >);
+};
 
-const Image = () => {
-    return <img src="./images/book-1.jpg" alt="Stone of Fire (ARKANE Book 1)" />;
-}
-const Title = () => {
-    return <h2>Stone of Fire (ARKANE Book 1)</h2>;
-}
-const Author = () => {
-    return <h4>J.F. Penn </h4>;
-}
 
 const root = ReactDom.createRoot(document.getElementById
     ('root'));
