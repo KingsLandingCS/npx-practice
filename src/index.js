53544db6bec22db89758774a0371e1752f95ef8f
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-30 03:20:36 +0500

    learning

--- src/index.js
+++ src/index.js
@@ -6,37 +6,34 @@ const books = [{
     author: "John Doe",
     title: "Interesting facts for curious minds",
     img: "./images/book-1.jpg",
+    id: 1,
 },
 
 {
     author: "Freida McFaddenFreida McFadden",
     title: "The Housemaid's Secret",
     img: "https://m.media-amazon.com/images/I/81LZXcfKqJL._AC_UL320_.jpg",
+    id: 2,
 },];
 
-const names = ["john", "peter", "susan"];
-const newNames = names.map((name) => {
-    return <h1>{name}</h1>;
-});
-
-console.log(newNames);
 
 function BookList() {
     return (
         <section className='booklist'>
-            {newNames}
+            {books.map((book) => {
+                return <Book {...book} key={book.id}></Book>
+            })}
         </section>
     );
 }
 
 const Book = ({ props }) => {
-    const { img, title, author } = props
+    const { img, title, author } = props;
     console.log(props);
     return (<article className="book">
         <img src={img} alt={title} />
         <h2>{title}</h2>
         <h4>{author.toUpperCase()}</h4>
-
     </article >);
 };
 
